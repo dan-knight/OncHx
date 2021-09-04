933a423c94e8a312c61cea679cebd806de05fec5
Daniel <56950335+thedank_night@users.noreply.github.com>
2021-09-03 20:49:30 -0700

    Setup basic styling
    - All styling lives in main.scss for now, but can be split up when
      needed.
    
    - Setup basic layout and neutral colours. This can be changed when
      a colour scheme is chosen (keeping depth from grayscale).

--- src/components/TreatmentInput.jsx
+++ src/components/TreatmentInput.jsx
@@ -1,15 +1,33 @@
-export default function TreatmentInput({ onSubmit }) {
+export default function TreatmentInput() {
   function handleSubmit(event) {
     event.preventDefault();
-    onSubmit('treatment');
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name='pid' />
-      <input name='date' type='date' />
-      <input name='treatment' />
-      <button type='submit'>Add Treatment</button>
-    </form>
+    <div className='form'>
+      <h3>Add Treatment</h3>
+      <form onSubmit={handleSubmit}>
+        <span>
+          <label>Cancer Type</label>
+          <select name='cancer-type'>
+            <option>Prostate</option>
+            <option>Brain</option>
+            <option>Lung</option>
+            <option>Breast</option>
+          </select>
+        </span>
+        <span>
+          <label>Date</label>
+          <input name='date' type='date' />
+        </span>
+        <span>
+          <label>Treatment Type</label>
+          <input name='treatment' />
+        </span>
+        <span className='button'>
+          <button type='submit'>Add Treatment</button>
+        </span>
+      </form>
+    </div>
   );
 }
