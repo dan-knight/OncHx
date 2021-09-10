f9d6e2503695b01dc9071611fdbc06ca96d3e5b4
Daniel <56950335+thedank_night@users.noreply.github.com>
2021-09-09 22:55:29 -0700

    Setup basic Formik forms
    - Created TreatmentInput component with some logic for date input
      and placeholder values for other fields.

--- src/components/TreatmentInput.jsx
+++ src/components/TreatmentInput.jsx
@@ -1,33 +1,82 @@
+import { Formik, Form, Field } from "formik";
+import { useState } from "react";
+
 export default function TreatmentInput() {
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+  function handleSubmit(values) {
+    alert(JSON.stringify(values, null, 2));
+  };
+  
+  const [today, setToday] = useState(new Date());
+
+  const [months, setMonths] = useState([
+    { name: 'January', days: 31 },
+    { name: 'February', days: 28 },
+    { name: 'March', days: 31 },
+    { name: 'April', days: 30 },
+    { name: 'May', days: 31 },
+    { name: 'June', days: 30 },
+    { name: 'July', days: 31 },
+    { name: 'August', days: 31 },
+    { name: 'September', days: 30 },
+    { name: 'October', days: 31 },
+    { name: 'November', days: 30 },
+    { name: 'December', days: 31 }
+  ]);
 
   return (
     <div className='form'>
       <h3>Add Treatment</h3>
-      <form onSubmit={handleSubmit}>
-        <span>
-          <label>Cancer Type</label>
-          <select name='cancer-type'>
-            <option>Prostate</option>
-            <option>Brain</option>
-            <option>Lung</option>
-            <option>Breast</option>
-          </select>
-        </span>
-        <span>
-          <label>Date</label>
-          <input name='date' type='date' />
-        </span>
-        <span>
-          <label>Treatment Type</label>
-          <input name='treatment' />
-        </span>
-        <span className='button'>
-          <button type='submit'>Add Treatment</button>
-        </span>
-      </form>
+      <Formik
+        initialValues={{
+          cancerType: '',
+          month: today.getMonth(),
+          day: today.getDay(),
+          year: today.getFullYear(),
+          treatmentType: ''
+        }}
+
+        onSubmit={handleSubmit}>
+        {({ values }) => (
+          <Form>
+            <span>
+              <label>Cancer Type</label>
+              <Field name='cancerType'>
+                {}
+              </Field>
+            </span>
+            <span>
+              <label>Month</label>
+              <Field name='month' as='select'>
+                {Object.keys(months).map(m => <option value={m} label={months[m].name} key={m} />)}
+              </Field>
+            </span>
+            <span>
+              <label>Day</label>
+              <Field name='day' as='select'>
+                {[...Array((months[values.month].days + 1) + (values.month == 1 && values.year % 4 == 0)).keys()].slice(1).map(d => (
+                  <option value={d} label={d} key={d} />
+                ))}
+              </Field>
+            </span>
+            <span>
+              <label>Year</label>
+              <Field name='year' as='select'>
+                {[...Array(today.getFullYear() + 1 - 1960).keys()].slice(1).map(y =>{
+                  const year = y + 1960;
+                  return <option value={year} label={year} />
+                })}
+              </Field>
+            </span>
+            <span>
+              <label>Treatment Type</label>
+              <Field name='treatmentType' />
+            </span>
+            <span className='button'>
+              <button type='submit'>Add Treatment</button>
+            </span>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
