dcc65bc2f1b72d76b4d62c381780b551629baad7
Daniel <56950335+thedank_night@users.noreply.github.com>
2021-09-12 22:24:52 -0700

    Setup basic routing
    - Used React Router
    
    - Moved placeholder page to /add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import TreatmentInput from './components/TreatmentInput';
 
 export default function App() {
   return (
-    <div className='app'>
-      <TreatmentInput />
-    </div>
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route path='/add'>
+            <TreatmentInput />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
