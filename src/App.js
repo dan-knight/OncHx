6793079b420ca93f239685d3b854a4dd5cb48665
Daniel <56950335+thedank_night@users.noreply.github.com>
2021-09-19 18:22:59 -0700

    Persist events in localStorage
    - Event dates must be turned into Date objects iteratively on import

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ import TreatmentInput from './components/TreatmentInput';
 import EventLog from './components/EventLog';
 
 export default function App() {
-  const [events, setEvents] = useState([
-    {
-      cancerType: 'Prostate', date: new Date(2020, 4, 15), treatmentType: 'Chemotherapy',
-      details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In turpis ante, elementum sed ligula ac, scelerisque cursus magna.'}
-  ]);
+  const [events, setEvents] = useState(getEvents());
+
+  function getEvents() {
+    const stored = JSON.parse(localStorage.getItem('events'));
+    
+    return stored ? stored.map(e => ({...e, date: new Date(e.date)})) : [
+      {
+        cancerType: 'Prostate', date: new Date(2020, 4, 15), treatmentType: 'Chemotherapy',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In turpis ante, elementum sed ligula ac, scelerisque cursus magna.'
+      }
+    ];
+  };
 
   function addEvent(e) {
-    setEvents([...events, e]);
+    const newEvents = [
+      {cancerType: 
+        e.cancerType, 
+        date: new Date(e.year, e.month, e.day),
+        treatmentType: e.treatmentType,
+        details: e.details
+      },
+      ...events];
+
+    localStorage.setItem('events', JSON.stringify(newEvents));
+    setEvents(newEvents);
   };
 
   return (
