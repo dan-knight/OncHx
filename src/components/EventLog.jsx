66f4d868f0d31fd5da1cf5e6a6cf55894322a663
Daniel <56950335+thedank_night@users.noreply.github.com>
2021-09-20 19:03:05 -0700

    Add option to show/hide event details
    - Used placeholder eventIDs until that can be added to App state

--- src/components/EventLog.jsx
+++ src/components/EventLog.jsx
@@ -3,10 +3,23 @@ import { Link } from "react-router-dom";
 
 export default function EventLog({ allEvents, user }) {
   const [events, setEvents] = useState(allEvents);
+  const [expanded, setExpanded] = useState(new Set());
 
   useState(() => {
     setEvents(allEvents.filter(e => e.user == user));
   }, [allEvents, user]);
+
+  function showHide(eventID) {
+    const newExpanded = new Set(expanded);
+
+    if (newExpanded.has(eventID)) {
+      newExpanded.delete(eventID);
+    } else {
+      newExpanded.add(eventID);
+    }
+
+    setExpanded(newExpanded);
+  };
 
   return (
     <div className='event-log'>
@@ -14,19 +27,25 @@ export default function EventLog({ allEvents, user }) {
         <button>Add Event</button>
       </Link>
       <ul>
-        {events.map(e => (
-          <Event date={e.date} cancer={e.cancerType} treatment={e.treatmentType} details={e.details}/>))}
+        {events.map((e, i) => (
+          <Event date={e.date} cancer={e.cancerType} treatment={e.treatmentType} details={e.details} 
+            eventID={i} show={expanded.has(i)} onShow={showHide} />))}
       </ul>
     </div>
   );
 };
 
-function Event({cancer, date, treatment, details}) {
+function Event({cancer, date, treatment, details, show, eventID, onShow}) {
+  function handleShow() {
+    onShow(eventID);
+  };
+
   return (
-    <div>
+    <li>
       <h5>{treatment}<span>{cancer}</span></h5>  
       <h4>{date.toDateString()}</h4>
-      <p>{details}</p>
-    </div>
+      <h6>Details <span onClick={handleShow}>{`(${show ? 'Hide' : 'Show'})`}</span></h6>
+      <p className={show ? 'expanded' : null}>{details}</p>
+    </li>
   );
 };
