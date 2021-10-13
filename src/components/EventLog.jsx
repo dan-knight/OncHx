c1a100bfaeaffbc03f8d90d6e0355112c435f0c9
Daniel <56950335+thedank_night@users.noreply.github.com>
2021-10-12 22:17:59 -0700

    Separate events by year

--- src/components/EventLog.jsx
+++ src/components/EventLog.jsx
@@ -2,12 +2,27 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function EventLog({ allEvents, user }) {
-  const [events, setEvents] = useState(allEvents);
+  const [events, setEvents] = useState({});
   const [expanded, setExpanded] = useState(new Set());
 
   function prepareEvents() {
-    const newEvents = [...allEvents].filter(e => e.user == user);
-    newEvents.sort((a, b) => b.date - a.date);
+    const newEvents = {};
+    
+    allEvents.forEach(e => {
+      if (e.user == user) {
+        const year = e.date.getFullYear();
+
+        if (newEvents[year] === undefined) {
+          newEvents[year] = [e];
+        } else {
+          newEvents[year].push(e);
+        }
+      }
+    });
+
+    Object.keys(newEvents).forEach(k => { 
+      newEvents[k].sort((a, b) => b.date - a.date);
+    });
 
     return newEvents;
   };
@@ -34,9 +49,21 @@ export default function EventLog({ allEvents, user }) {
         <button>Add Event</button>
       </Link>
       <ul>
+        {Object.entries(events).sort((a, b) => b[0] - a[0]).map(e => (
+          <EventYear year={e[0]} events={e[1]} key={e[0]} expanded={expanded} onShow={showHide} />))}
+      </ul>
+    </div>
+  );
+};
+
+function EventYear({year, events, expanded, onShow}) {
+  return (
+    <div>
+      <h1>{year}</h1>
+      <ul>
         {events.map((e, i) => (
           <Event date={e.date} cancer={e.cancerType} treatment={e.treatmentType} details={e.details} 
-            eventID={i} show={expanded.has(i)} onShow={showHide} />))}
+            eventID={i} show={expanded.has(i)} onShow={onShow} />))}
       </ul>
     </div>
   );
