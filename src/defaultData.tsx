f449ec9f6ecada29cd0b68471c96fcc8aca2b486
Daniel <56950335+dan-knight@users.noreply.github.com>
2022-02-20 10:46:06 -0800

    Update default events with new data structures

--- src/defaultData.tsx
+++ src/defaultData.tsx
@@ -1,9 +1,17 @@
-import { PatientEvent } from "./types/Event";
+import DBPatientEvent from "./types/PatientEvent/DBPatientEvent";
+import DetailValuesFactory from "./types/PatientEvent/Details/DetailValuesFactory";
+import { TreatmentTypeName, TreatmentTypeNameIndex } from "./config/TreatmentTypeName";
+import { EventDetailValues } from "./types/PatientEvent/Details/EventDetailValues";
 
-export const defaultEvents = (): PatientEvent[] => ([
-  new PatientEvent('patient', 0, new Date(2020, 10, 15), 0),
-  new PatientEvent('patient', 15, new Date(2020, 10, 18), 1),
-  new PatientEvent('patient', 13, new Date(2020, 10, 18), 0),
-  new PatientEvent('patient', 20, new Date(2021, 1, 18), 0),
-  new PatientEvent('patient', 1, new Date(2021, 1, 8), 1, { notes: 'Note' })
-]);
+export const defaultEvents = (): DBPatientEvent[] => {
+  const treatmentType: TreatmentTypeName = TreatmentTypeNameIndex[0];
+  const details: EventDetailValues = DetailValuesFactory.createDetails(treatmentType);
+
+  return [
+    new DBPatientEvent(0, 'patient', details, new Date(2020, 10, 15), 0, 0),
+    new DBPatientEvent(1, 'patient', details, new Date(2020, 10, 18), 1, 15),
+    new DBPatientEvent(2, 'patient', details, new Date(2020, 10, 18), 0, 13),
+    new DBPatientEvent(3, 'patient', details, new Date(2021, 1, 18), 0, 20),
+    new DBPatientEvent(4, 'patient', details, new Date(2021, 1, 8), 1, 1)
+  ];
+};
