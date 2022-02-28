099a58151dbcc79a3992f2cfb31537d1690bc68d
Daniel <56950335+dan-knight@users.noreply.github.com>
2022-02-27 17:23:09 -0800

    Move generic constructor utility type to separate file

--- src/types/PatientEvent/Details/DetailFieldsFactory.ts
+++ src/types/PatientEvent/Details/DetailFieldsFactory.ts
@@ -1,20 +1,17 @@
-import TreatmentType from "../../DB/Config/TreatmentType";
-import { DetailField } from "./DetailField";
+import { GenericConstructor } from "../../utility/Constructor";
 import { EventDetailFields } from "./EventDetailFields";
 import { ChemotherapyDetailFields } from "./EventTypes/ChemotherapyDetails";
 import { RadiationDetailFields } from "./EventTypes/RadiationDetails";
 
-type GConstructor<T extends {}> = new (...args: any[]) => T;
-
 export default class DetailFieldsFactory {
-  private static readonly treatmentTypes: Record<number, GConstructor<EventDetailFields>> = {
+  private static readonly treatmentTypes: Record<number, GenericConstructor<EventDetailFields>> = {
     0: ChemotherapyDetailFields,
     1: RadiationDetailFields,
     2: ChemotherapyDetailFields
   };
 
   static createFields(treatmentTypeID?: number): EventDetailFields {
-    const DetailFieldType: GConstructor<EventDetailFields> | undefined = treatmentTypeID !== undefined ? 
+    const DetailFieldType: GenericConstructor<EventDetailFields> | undefined = treatmentTypeID !== undefined ? 
       DetailFieldsFactory.treatmentTypes[treatmentTypeID] : undefined;
     
     return DetailFieldType !== undefined ? new DetailFieldType() : {};
