c67d189899d7fcdc0f9f0f3422e6806ec919fa82
Daniel <56950335+dan-knight@users.noreply.github.com>
2022-03-05 19:17:01 -0800

    Create default patient and update PatientEvent patientID data type

--- src/types/PatientEvent/PatientEvent.ts
+++ src/types/PatientEvent/PatientEvent.ts
@@ -2,15 +2,15 @@ import { EventDetailValues } from "./Details/EventDetailValues";
 import Event from "./Event";
 
 export default class PatientEvent extends Event {
-  patient: string;
+  patient: number;
   details: EventDetailValues;
   treatmentType?: number;
   cancerType?: number;
 
-  constructor(patient: string, details: EventDetailValues, date?: Date, treatmentType?: number, cancerType?: number);
-  constructor(patient: string, details: EventDetailValues, date?: string, treatmentType?: number, cancerType?: number);
-  constructor(patient: string, details: EventDetailValues, date?: Date | string, treatmentType?: number, cancerType?: number);
-  constructor(patient: string, details: EventDetailValues, date?: Date | string, treatmentType?: number, cancerType?: number) {
+  constructor(patient: number, details: EventDetailValues, date?: Date, treatmentType?: number, cancerType?: number);
+  constructor(patient: number, details: EventDetailValues, date?: string, treatmentType?: number, cancerType?: number);
+  constructor(patient: number, details: EventDetailValues, date?: Date | string, treatmentType?: number, cancerType?: number);
+  constructor(patient: number, details: EventDetailValues, date?: Date | string, treatmentType?: number, cancerType?: number) {
     
     try {
       (super(typeof date === 'string' ? new Date(Date.parse(date)) : date)); 
