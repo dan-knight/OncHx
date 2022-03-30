44ccebb26ae0950df01af91d1632a1a2fcbce284
Daniel <56950335+dan-knight@users.noreply.github.com>
2022-03-29 19:56:58 -0700

    Create login handling function

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,16 +3,29 @@ import * as Yup from 'yup';
 import { useGlobalContext } from "../contexts/GlobalContext";
 import { GlobalValues } from "../types/Global";
 import LoginValues from "../types/Login";
+import Patient from "../types/Patient/Patient";
 import TextField from "./form/TextField";
 
 const treatmentSchema = Yup.object().shape({
   email: Yup.string().required('Required').email(),
   password: Yup.string().required('Required')
 });
+
 export default function Login() {
-  function handleLogin(values: LoginValues) {
-    console.log(values);
+  const { getPatientIndex, patients, login }: GlobalValues = useGlobalContext();
+
+  function handleLogin(values: LoginValues, { setFieldError, setValues }: FormikValues) {
+    const userIndex: number | undefined = getPatientIndex(values.email);
+
+    if (userIndex !== undefined && values.password === patients?.[userIndex].password) {
+      login(userIndex);
+    } else {
+      setFieldError('email', 'Invalid email/password combination');
+    }
+
+    setValues({ email: '', password: '' })
   }
+
   return (
     <div className='form'>
       <h3>Login</h3>
